8a3073c60e3506226f7c70f3b32c4ec7ec6d803e
Jean-Pierre von der Heydt <usgva@student.kit.edu>
2022-05-16 10:02:33 +0200

    Compile information for bigger test sets

--- test_scripts/compile_planDD_statistics.py
+++ test_scripts/compile_planDD_statistics.py
@@ -2,13 +2,15 @@ from extract_planDD_information import *
 
 import statistics
 
-def print_information_from_dicts(all_dics):
+# print information if only a single run is of interest
+def print_big_information_from_dicts(all_dics):
     non_unit_length_dics = [d for d in all_dics if d["error_while_encoding"]]
     not_finished_cnf_dics = [d for d in all_dics if not d["error_while_encoding"] and not d["has_finished_cnf"]]
     not_finished_dd_dics = [d for d in all_dics if not d["error_while_encoding"] and d["has_finished_cnf"] and not d["has_finished"]]
     solved_dics = [d for d in all_dics if not d["error_while_encoding"] and d["has_finished_cnf"] and d["has_finished"]]
 
-    print(sorted([i["domain_desc"] for i in solved_dics]))
+    print("Compiling Information: ########################################################")
+    #print(sorted([i["domain_desc"] for i in solved_dics]))
 
     print("#Total", len(all_dics))
     print("#Non unit cost", len(non_unit_length_dics))
@@ -37,6 +39,24 @@ def print_information_from_dicts(all_dics):
     average_bdd_nodes = statistics.mean([get_number_of_nodes_from_bdd_from_info(i) for i in solved_dics])
     print("Average number of nodes for bdd on solved test cases {:0.0f}".format(average_bdd_nodes))
 
+# print information in multiple suites are of interest. 
+# Have to modify the function i a way that ist also prints the difference between the suites (e.g. variable/conjoin order)
+def get_small_information_from_dicts(all_dics):
+    conjoin_order = get_conjoin_order_from_info(all_dics[0])
+    solved_dics = [d for d in all_dics if not d["error_while_encoding"] and d["has_finished_cnf"] and d["has_finished"]]
+
+    average_peak_of_nodes = statistics.mean([get_peak_number_of_nodes_from_info(i) for i in solved_dics])
+    #print("Average peak of nodes on solved test cases {:0.0f}".format(average_peak_of_nodes))
+    average_peak_of_live_nodes = statistics.mean([get_peak_number_of_live_nodes_from_info(i) for i in solved_dics])
+    #print("Average peak of live nodes on solved test cases {:0.0f}".format(average_peak_of_live_nodes))
+    average_bdd_nodes = statistics.mean([get_number_of_nodes_from_bdd_from_info(i) for i in solved_dics])
+    #print("Average number of nodes for bdd on solved test cases {:0.0f}".format(average_bdd_nodes))
+
+    info_string = "Conjoin Order: {}, #Solved: {}, Avg. peak nodes {:0.0f}, Avg. live nodes {:0.0f}, Avg. BDD size {:0.0f}".format(conjoin_order, len(solved_dics), average_peak_of_nodes, average_peak_of_live_nodes, average_bdd_nodes) 
+
+    return (len(solved_dics), info_string)
+
+
 
 
 #write_all_information_to_file("../test_output/small_easy_optimal_planDD_test", "../test_output/small_easy_optimal_planDD_test.pkl")
@@ -44,8 +64,17 @@ def print_information_from_dicts(all_dics):
 #downward_write_all_information_to_file("easy_optimal_downward_test", "../test_output/easy_optimal_downward_test.pkl")
 
 #small_planDD_dics = read_all_information_from_file("../test_output/small_easy_optimal_planDD_test.pkl")
-planDD_dics = read_all_information_from_file("../test_output/easy_optimal_planDD_test.pkl")
+#planDD_dics = read_all_information_from_file("../test_output/easy_optimal_planDD_test.pkl")
 #print_information_from_dicts(small_planDD_dics)
-print_information_from_dicts(planDD_dics)
-downward_dics = read_all_information_from_file("../test_output/easy_optimal_downward_test.pkl")
+#print_information_from_dicts(planDD_dics)
+#downward_dics = read_all_information_from_file("../test_output/easy_optimal_downward_test.pkl")
 #print(downward_dics)
+
+suites = read_whole_set_of_tests_from_file("../test_output/big_interleaved")
+compiled_infos = []
+
+for s in suites:
+    compiled_infos.append(get_small_information_from_dicts(s))
+
+for (num_solved, info_text) in sorted(compiled_infos):
+    print(info_text)
